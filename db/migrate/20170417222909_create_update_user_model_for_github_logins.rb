ac2a16c9d231c2db2b711384d7e2b58ae7397b01
Tamiko Terada <tamikodevo@gmail.com>
2017-04-17 15:32:16 -0700

    fixed typo

--- db/migrate/20170417222909_create_update_user_model_for_github_logins.rb
+++ db/migrate/20170417222909_create_update_user_model_for_github_logins.rb
@@ -1,8 +1,7 @@
 class CreateUpdateUserModelForGithubLogins < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :oauth_uid, :string
+    add_column :users, :git_uid, :string
     add_column :users, :email, :string
     add_column :users, :provider, :string
-    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170407164321) do
+ActiveRecord::Schema.define(version: 20170417222909) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,9 @@ ActiveRecord::Schema.define(version: 20170407164321) do
     t.string   "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "git_uid"
+    t.string   "email"
+    t.string   "provider"
   end
 
   create_table "votes", force: :cascade do |t|
