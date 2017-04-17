da976d9984bc16b17387aae5c5c9973bf456aebf
Tamiko Terada <tamikodevo@gmail.com>
2017-04-17 15:49:17 -0700

    update User#github_callback

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,5 +11,26 @@ class UsersController < ApplicationController
   def github_callback
     auth_hash = request.env['omniauth.auth']
 
-    raise
+    user = User.find_by(provider: params[:provider], outh_uid: auth_hash['uid'])
+
+    if user.nil?
+      # new User is created
+      user = User.from_github(auth_hash)
+
+      if user.save
+        session[:user_id] = user.id
+        flash[:message] = "Welcome, #{user.username}!"
+      else
+        flash[:message] = "Couldn't log in."
+        user.errors.messages.each do |field, problem|
+          flash[:field] = problem.join(', ')
+        end
+      end
+
+    else
+      session[:user_id] = user.id
+      flash[:message] = "Welcome back, #{user.username}"
+    end
+    redirect_to root_path
+  end
 end
