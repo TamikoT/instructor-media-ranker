425db6ed9a11acc2b464e3f288c3660d17647653
Tamiko Terada <tamikodevo@gmail.com>
2017-04-17 15:37:51 -0700

    add omniauth.rb to config OAuth info

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,4 @@
+# references hidden .env file to configure App Oauth info
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :github, ENV["GITHUB_CLIENT_ID"], ENV["GITHUB_CLIENT_SECRET"], scope: "user:email"
+end
